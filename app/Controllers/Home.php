496ee37de349b7e700e051c0a73d28cbd3978dc6
venkat nikhil <vnikhil955@gmail.com>
2021-07-02 11:32:04 +0530

    commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,18 @@ class Home extends BaseController
 	{
 		return view('welcome_message');
 	}
+	public function rewriter()
+	{
+		return view('rewriter.php');
+	}
+	public function rewriter1()
+	{
+		return view('create_form.php');
+	}
+	public function create_textbox()
+	{
+		return view('create_textbox.php');
+	}
+
+
 }
