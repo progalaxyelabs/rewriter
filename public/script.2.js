8ab864f3839cbdde3d1b1ecd5e77db89814189f1
pradeepdsmk <pradeepdsmk@gmail.com>
2021-07-03 14:25:32 +0530

    modified script.2.js - altered loader function sequence to help adding new modules at the end

--- public/script.2.js
+++ public/script.2.js
@@ -1,38 +1,43 @@
-var HomeCreateForm = function () {
-    let isInitialized = false;
-    let init = function () {
-        if(isInitialized) {
-            return;
-        }
-
-        console.log('HomeCreateForm module initialized');
-        isInitialized = true;
-    }
-
-    return {
-        init: init
-    };
-};
-
 window.addEventListener('DOMContentLoaded', (e) => {
     let pageModuleElements = document.querySelectorAll('.screen-js-module');
-    if(!pageModuleElements) {
+    if (!pageModuleElements) {
         return;
     }
 
-    for(let i = 0; i < pageModuleElements.length; i++) {
+    for (let i = 0; i < pageModuleElements.length; i++) {
         let element = pageModuleElements[i];
         let moduleName = element.getAttribute('data-module');
-        
-        if(!moduleName) {
+
+        if (!moduleName) {
             continue;
         }
 
-        if(!window[moduleName]) {
-            continue;            
+        if (!window[moduleName]) {
+            continue;
         }
 
         let module = window[moduleName]();
         module.init();
     }
 });
+
+var HomeCreateForm = function () {
+    let isInitialized = false;
+    let init = function () {
+        if (isInitialized) {
+            return;
+        }
+
+        bindUiActions();
+
+        isInitialized = true;
+    };
+
+    let bindUiActions = function () {
+        console.log('HomeCreateForm module initialized');
+    };
+
+    return {
+        init: init
+    };
+};
