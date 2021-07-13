cc0a43f2cb1d60c2dc34ec77c50ae432e161a18c
pradeepdsmk <pradeepdsmk@gmail.com>
2021-07-13 16:09:35 +0530

    added db.sql with templates, screens and forms table definitions

---
+++ app/Database/Seeds/db.sql
@@ -0,0 +1,27 @@
+create table templates (
+    id int not null auto_increment,
+    name varchar(20) not null,
+    created_at datetime not null default current_timestamp(),
+    last_updated_at datetime not null default current_timestamp() on update current_timestamp(),
+    primary key(id)
+);
+insert into templates (name)
+values ("Grocery Stores"),
+    ("Restaurant"),
+    ("Function Hall");
+create table screens (
+    id int not null auto_increment,
+    name varchar(20) not null,
+    template_id int not null,
+    created_at datetime not null default current_timestamp(),
+    last_updated_at datetime not null default current_timestamp() on update current_timestamp(),
+    primary key(id)
+);
+create table forms (
+    id int not null auto_increment,
+    name varchar(20) not null,
+    screen_id int not null,
+    created_at datetime not null default current_timestamp(),
+    last_updated_at datetime not null default current_timestamp() on update current_timestamp(),
+    primary key(id),
+);
