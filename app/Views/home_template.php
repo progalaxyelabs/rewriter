cc1d7c4efd86d554013ff01ffe7a3f21eec1eca9
pradeepdsmk <pradeepdsmk@gmail.com>
2021-07-13 17:49:21 +0530

    rearranged home_template view file to bring the screen links inside bootstrap container

--- app/Views/home_template.php
+++ app/Views/home_template.php
@@ -13,42 +13,44 @@
                 <a id="new_screen_anchor" onclick="showOff()" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalForNewScreen" role="button">
                     New Screen</a>
             </div>
-
-            <div class="modal fade" id="ModalForNewScreen" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">New Screen</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <form method="post" action="/home/new_screen_submit">
-                            <div class="modal-body">
-                                <label for="modal">Name:</label>
-                                <input class="form-control" id="newScreenInput" type="text" name="screen_name">
-                                <input type="hidden" name="template_id" value="<?= $template_screens[0]->template_id ?>">
-                            </div>
-                            <div class="modal-footer">
-                                <button id="submit" type="submit" class="btn btn-primary">Submit</button>
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
         </div>
+    </div>
+
+    <div class="row">
+        <?php foreach ($template_screens as $screen) : ?>
+            <?php if (!empty($screen->screen_id)) : ?>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2 screen-link-wrap">
+                    <a class="btn btn-primary" href="/home/screen?screen_id=<?= $screen->screen_id ?>">
+                        <?= $screen->screen_name ?>
+                    </a>
+                </div>
+            <?php endif; ?>
+        <?php endforeach; ?>
     </div>
 </div>
 
-<div class="row">
-    <?php foreach ($template_screens as $screen) : ?>
-        <?php if (!empty($screen->screen_id)) : ?>
-            <div class="col-6 col-sm-4 col-md-3 col-lg-2 screen-link-wrap">
-                <a class="btn btn-primary" href="/home/screen?screen_id=<?= $screen->screen_id ?>">
-                    <?= $screen->screen_name ?>
-                </a>
+
+
+<div class="modal fade" id="ModalForNewScreen" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">New Screen</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-        <?php endif; ?>
-    <?php endforeach; ?>
+            <form method="post" action="/home/new_screen_submit">
+                <div class="modal-body">
+                    <label for="modal">Name:</label>
+                    <input class="form-control" id="newScreenInput" type="text" name="screen_name">
+                    <input type="hidden" name="template_id" value="<?= $template_screens[0]->template_id ?>">
+                </div>
+                <div class="modal-footer">
+                    <button id="submit" type="submit" class="btn btn-primary">Submit</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
 
