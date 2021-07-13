b7468333eeeea226f38b6ff6d21d7603eaad17c6
pradeepdsmk <pradeepdsmk@gmail.com>
2021-07-13 16:28:38 +0530

    formatted long sql select queries, used multiline and visible without scrolling

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -21,12 +21,20 @@ class Home extends BaseController
 		$templates_query = $this->db->query('select * from templates');
 		$templates = $templates_query->getResult();
 		return view('home_all_templates', ['templates' => $templates],);
-		
 	}
 	public function template()
 	{
 		$template_id = filter_input(INPUT_GET, 'template_id', FILTER_SANITIZE_STRING);
-		$template_screens = $this->db->query('select t.id as template_id, t.name as template_name, s.id as screen_id, s.name as screen_name from templates t left join screens s on s.template_id = t.id where t.id = ?', [$template_id])->getResult();
+		$sql = "
+		select t.id as template_id,
+		t.name as template_name,
+		s.id as screen_id,
+		s.name as screen_name
+		from templates t
+		left join screens s on s.template_id = t.id
+		where t.id = ?
+		";
+		$template_screens = $this->db->query($sql, [$template_id])->getResult();
 		return view('home_template', ['template_screens' => $template_screens]);
 	}
 	public function new_template_submit()
@@ -41,7 +49,7 @@ class Home extends BaseController
 	{
 		$screen_name = filter_input(INPUT_POST, 'screen_name', FILTER_SANITIZE_STRING);
 		$template_id = filter_input(INPUT_POST, 'template_id', FILTER_SANITIZE_STRING);
-		$screen = ['name' => $screen_name,'template_id' => $template_id];
+		$screen = ['name' => $screen_name, 'template_id' => $template_id];
 		$this->db->table('screens')->insert($screen);
 		$screen_id = $this->db->insertID();
 		return redirect()->to('/home/screen?screen_id=' . $screen_id);
@@ -51,9 +59,9 @@ class Home extends BaseController
 		$form_name = filter_input(INPUT_POST, 'form_name', FILTER_SANITIZE_STRING);
 		$screen_id = filter_input(INPUT_POST, 'screen_id', FILTER_SANITIZE_STRING);
 		$form = [
-    				'name' => $form_name,
-    				'screen_id' => $screen_id
-				];
+			'name' => $form_name,
+			'screen_id' => $screen_id
+		];
 		$this->db->table('forms')->insert($form);
 		$form_id = $this->db->insertID();
 		return redirect()->to('/home/form?form_id=' . $form_id);
@@ -61,14 +69,34 @@ class Home extends BaseController
 	public function screen()
 	{
 		$screen_id = filter_input(INPUT_GET, 'screen_id', FILTER_SANITIZE_STRING);
-		$screen = $this->db->query('select s.id as screen_id, s.name as screen_name, s.template_id, t.name as template_name from screens s join templates t on s.template_id = t.id where s.id = ?', [$screen_id])->getRow();
+		$sql = "
+		select s.id as screen_id,
+		s.name as screen_name,
+		s.template_id,
+		t.name as template_name
+		from screens s
+		join templates t on s.template_id = t.id
+		where s.id = ?
+		";
+		$screen = $this->db->query($sql, [$screen_id])->getRow();
 		return view('home_screen', ['screen' => $screen]);
 	}
 	public function form()
 	{
 		$form_id = filter_input(INPUT_GET, 'form_id', FILTER_SANITIZE_STRING);
-		$form = $this->db->query('select f.id as form_id, f.name as form_name, s.id as screen_id, s.name as screen_name, t.id as template_id, t.name as template_name from forms f join screens s on s.id = f.screen_id join templates t on t.id = s.template_id where f.id = ?', [$form_id])->getRow();
+		$sql = "
+		select f.id as form_id,
+		f.name as form_name,
+		s.id as screen_id,
+		s.name as screen_name,
+		t.id as template_id,
+		t.name as template_name
+		from forms f
+		join screens s on s.id = f.screen_id
+		join templates t on t.id = s.template_id
+		where f.id = ?
+		";
+		$form = $this->db->query($sql, [$form_id])->getRow();
 		return view('home_form', ['form' => $form]);
 	}
-
 }
