e00391786aeeae2e0c69cc011502d5abf39a82f9
pradeepdsmk <pradeepdsmk@gmail.com>
2021-07-27 17:03:08 +0530

    screens: rearrange buttons in a horizontal scrollable container. combine all css and js files and remove unwanted files

--- app/Views/home_form.php
+++ app/Views/home_form.php
@@ -15,8 +15,7 @@
 
                 <div class="row screen-buttons">
                     <div class="col">
-                        <a id="back_to_template" class="btn btn-primary"
-                            href="/home/screen?generic_screen_id=<?= $form->generic_screen_id ?>">Back to screen</a>
+                        <a id="back_to_template" class="btn btn-primary" href="/home/screen?generic_screen_id=<?= $form->generic_screen_id ?>">Back to screen</a>
                     </div>
                     <div class="col"></div>
                     <div class="col">
@@ -27,42 +26,27 @@
             </div>
 
             <div id="formButtonBorder" class="col-md-8 offset-md-2 text-center">
-                <div id="create_buttons" class="row screen-buttons">
+                <div id="create_buttons" class="screen-buttons">
 
-                    <div id="form_buttons" class="col">
-                        <button id="date" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Date</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button id="name" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Simple Text</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button id="number-input" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Number</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button id="email" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Email</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button id="go_to_screen" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1" type="button">Go to link</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button id="options" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2" type="button">Options</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button id="create-password" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button"
-                            class="btn btn-primary">Password</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button id="create-checkbox" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button"
-                            class="btn btn-primary">Tick Box</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" id="longtextarea"
-                            class="btn btn-primary">Paragraph</button>
-                    </div>
-                    <div id="form_buttons" class="col">
-                        <button id="create-button" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button"
-                            class="btn btn-primary">Button</button>
-                    </div>
+                    <button id="date" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Date</button>
+
+                    <button id="name" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Simple Text</button>
+
+                    <button id="number-input" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Number</button>
+
+                    <button id="email" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Email</button>
+
+                    <button id="go_to_screen" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1" type="button">Go to link</button>
+
+                    <button id="options" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2" type="button">Options</button>
+
+                    <button id="create-password" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">Password</button>
+
+                    <button id="create-checkbox" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">Tick Box</button>
+
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" id="longtextarea" class="btn btn-primary">Paragraph</button>
+
+                    <button id="create-button" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">Button</button>
 
                 </div>
             </div>
@@ -87,8 +71,7 @@
             </div>
         </div>
 
-        <div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="homeFormModalLabel1"
-            aria-hidden="true">
+        <div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="homeFormModalLabel1" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -122,8 +105,7 @@
             </div>
         </div>
 
-        <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="homeFormModalLabel1"
-            aria-hidden="true">
+        <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="homeFormModalLabel1" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -136,7 +118,7 @@
                             <input id="optionLabel" class="form-control" type="text">
                         </div>
                         <div id="optionModalBody">
-                        <label for="optionIput">Enter Option Name:</label><br>
+                            <label for="optionIput">Enter Option Name:</label><br>
                             <div class="input-group mb-3">
                                 <input id="optionInput" type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
                                 <div class="input-group-append">
@@ -157,7 +139,7 @@
         </div>
 
         <div class="wrap row justify-content-center">
-        <form id="frame" class="form col col-sm-10 col-md-8 my-3"></form>
+            <form id="frame" class="form col col-sm-10 col-md-8 my-3"></form>
         </div>
     </div>
 
--- app/Views/templates.php
+++ app/Views/templates.php
@@ -9,8 +9,7 @@
     <title>Rewriter</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="/style.1.css">
-    <link rel="stylesheet" href="/style.2.css">
+    <link rel="stylesheet" href="/style.3.css">
 </head>
 
 <body>
@@ -39,8 +38,7 @@
     <?= $this->renderSection('content') ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="/form-controls.js"></script>
-    <script src="/script.1.js"></script>
-    <script src="/script.2.js"></script>    
+    <script src="/script.3.js"></script>    
 </body>
 
 </html>
